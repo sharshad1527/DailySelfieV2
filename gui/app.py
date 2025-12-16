204aadbb1ee0173d2bad88a19ed75aa94bf82ad2
Harshad <145334791+sharshad1527@users.noreply.github.com>
2025-12-16 17:01:44 +0530

    Removed

--- gui/app.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PySide6.QtWidgets import QApplication
-from startup.startup_window import StartupWindow
-
-app = QApplication(sys.argv)
-win = StartupWindow()
-win.show()
-sys.exit(app.exec())
