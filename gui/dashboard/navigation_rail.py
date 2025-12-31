a08c9a9e83387f1b5342aaf59e874a9316d4eaf0
Harshad <145334791+sharshad1527@users.noreply.github.com>
2025-12-31 12:30:08 +0530

    feat: introduce menu button type, refactor navigation rail button management and styling, and update calendar icons.

--- gui/dashboard/navigation_rail.py
+++ gui/dashboard/navigation_rail.py
@@ -24,8 +24,8 @@ ico_menu_open = "menu_open.svg"
 ico_dashboard = "dashboard.svg"
 ico_dashboard_filled = "dashboard_filled.svg"
 
-ico_calendar = "calendar.svg"
-ico_calendar_filled = "calendar_filled.svg"
+ico_calendar = "today.svg"
+ico_calendar_filled = "today_filled.svg"
 
 ico_settings = "settings.svg"
 ico_settings_filled = "settings_filled.svg"
@@ -33,11 +33,12 @@ ico_settings_filled = "settings_filled.svg"
 class NavButton(QWidget):
     clicked = Signal() # Add a Click Signal
     
-    def __init__(self, icon_normal, icon_checked, text: str = ""):
+    def __init__(self, icon_normal, icon_checked, text: str = "", isbtnmenu: bool = False) -> None:
         super().__init__()
         self._hovered = False
         self._checked = False
         self._collapsed = False
+        self._isbtnmenu = isbtnmenu
 
         self.setAttribute(Qt.WA_Hover, True)
         self.setAttribute(Qt.WA_StyledBackground, True) # Ensure background is painted
@@ -49,7 +50,7 @@ class NavButton(QWidget):
         self.icon_checked = icon_checked
 
         nav_button_layout = QHBoxLayout()
-        nav_button_layout.setContentsMargins(12, 12, 0, 10)
+        nav_button_layout.setContentsMargins(12, 12, 10, 10)
         nav_button_layout.setSpacing(0)
 
         self.icon_label = QLabel()
@@ -63,27 +64,30 @@ class NavButton(QWidget):
         self.setLayout(nav_button_layout)
         self.updateStyle()
 
+
     # Emit the clicked signal when the navbutton is clicked
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
-            print("NavButton clicked")
+            # print("NavButton clicked")
             self.clicked.emit()
+        
 
     # Emit the hovered signal when the navbutton is hovered
     def enterEvent(self, event):
-        print("NavButton entered")
+        # print("NavButton entered")
         self._hovered = True
         self.updateStyle()
 
     # Emit the unhovered signal when the navbutton is unhovered
     def leaveEvent(self, event):
-        print("NavButton left")
+        # print("NavButton left")
         self._hovered = False
         self.updateStyle()   
 
     # Set the checked state of the navbutton
     def setChecked(self, value: bool):
         self._checked = value
+        print("Checked: ", value)
         self.updateStyle()
 
     # Get the checked state of the navbutton
@@ -93,22 +97,31 @@ class NavButton(QWidget):
     # Set the collapsed state of the navbutton
     def setCollapsed(self, value: bool):
         self._collapsed = value
-        if value == True:
-            self.txt_label.hide()
-            self.icon_label.setPixmap(self.icon_normal.pixmap(24, 24))
-
-        else:
-            self.txt_label.show()
-            self.icon_label.setPixmap(self.icon_checked.pixmap(24, 24))
-
-
+        print("Collapsed: ", value)
+        self.updateStyle()
+            
     # Get the collapsed state of the navbutton
     def isCollapsed(self) -> bool:
         return self._collapsed
 
+
     # Update the style of the navbutton
     def updateStyle(self):
-        if self._checked:
+        # Style For Menu Button
+        if self._isbtnmenu  == True:
+            self.setStyleSheet(f"""
+                QWidget#NavButton {{ 
+                    background-color: transparent; 
+                    border-radius: 20px; 
+                }}
+                QLabel {{ 
+                    color: transparent; 
+                    background-color: transparent; 
+                }}
+            """)
+        
+        # Style For Button When Checked
+        elif self._checked:
             # CHECKED STATE
             # Material 3 Rule: Active/Selected items use the Secondary Container color.
             # Color Role: Secondary Container (Background)
@@ -119,10 +132,12 @@ class NavButton(QWidget):
                     border-radius: 20px; 
                 }}
                 QLabel {{ 
-                    color: {vars['on_secondary_container']}; 
+                    color: {vars['on_surface']}; 
                     background-color: transparent; 
                 }}
             """)
+        
+        # Style For Button When Hovered
         elif self._hovered:
             # HOVER STATE
             # Material 3 Rule: Hover state uses a state layer (usually 8% opacity on surface).
@@ -138,6 +153,8 @@ class NavButton(QWidget):
                     background-color: transparent; 
                 }}
             """)
+        
+        # Style For Button When Default
         else:
             # DEFAULT STATE
             # Material 3 Rule: Inactive items sit on the surface (transparent container).
@@ -154,6 +171,30 @@ class NavButton(QWidget):
                 }}
             """)
 
+        # Menu Button OR NavButton
+        if self._isbtnmenu == True:
+            # COLLAPSED STATE For Menu Button
+            if self._collapsed == False:
+                self.txt_label.hide()
+                self.icon_label.setPixmap(self.icon_checked.pixmap(24, 24))
+            else:
+                self.txt_label.hide()
+                self.icon_label.setPixmap(self.icon_normal.pixmap(24, 24))
+        else:
+            # Checked Or Collapsed
+            if self._checked == False:
+                self.icon_label.setPixmap(self.icon_normal.pixmap(24, 24))
+            else:
+                self.icon_label.setPixmap(self.icon_checked.pixmap(24, 24))
+
+            # COLLAPSED STATE For Button 
+            if self._collapsed == True:
+                self.txt_label.hide()
+            else:
+                self.txt_label.show()
+
+        
+
 
 class NavigationRail(QWidget):
     # Just Changed Value to low because of testing
@@ -165,54 +206,59 @@ class NavigationRail(QWidget):
         
         vars = theme_vars()
         self._is_collapsed = False
+        self._buttons = []
     
 
         vlayout = QVBoxLayout(self)
-        vlayout.setContentsMargins(0, 0, 0, 0)
+        vlayout.setContentsMargins(0, 0, 10, 10)
         vlayout.setSpacing(0)
 
-        v = theme_vars()
+        self._apply_icon_theme()
 
-        # Define colors
-        color_inactive = v.qcolor("on_surface_variant")
-        color_active = v.qcolor("primary")
-        # Shutter icon sits on 'primary' background, so use 'on_primary' (usually white/black)
-        color_on_primary = v.qcolor("on_primary")
-        
-        ico_menu = self._create_colored_icon("menu.svg", color_inactive)
-        ico_menu_open = self._create_colored_icon("menu_open.svg", color_active)
+        btn_menu = NavButton(self.ico_menu, self.ico_menu_open, "", True)
+        self._buttons.append(btn_menu)
+        btn_menu.clicked.connect(self.toggleCollapsedState)
+        vlayout.addWidget(btn_menu)
 
-        
-        self.sample = NavButton(ico_menu, ico_menu_open, "Menu")
-        self.sample.clicked.connect(self.toggleCollapsedState)
-        # self.sample.setChecked(True)
-        # sample.setCollapsed(True)
-        vlayout.addWidget(self.sample)
+        btn_dashboard = NavButton(self.ico_dashboard, self.ico_dashboard_filled, "Dashboard")
+        self._buttons.append(btn_dashboard)
+        btn_dashboard.clicked.connect(lambda: print("Dashboard clicked"))
+        vlayout.addWidget(btn_dashboard)
+
+        btn_calendar = NavButton(self.ico_calendar, self.ico_calendar_filled, "Calendar")
+        self._buttons.append(btn_calendar)
+        btn_calendar.clicked.connect(lambda: print("Calendar clicked"))
+        vlayout.addWidget(btn_calendar)
 
         vlayout.addStretch()
 
+        btn_settings = NavButton(self.ico_settings, self.ico_settings_filled, "Settings")
+        self._buttons.append(btn_settings)
+        btn_settings.clicked.connect(lambda: print("Settings clicked"))
+        vlayout.addWidget(btn_settings)
+
+        btn_dashboard.setChecked(True) # Defaultly Selected
 
         self.applyCollapsedState(self._is_collapsed)
 
-        # self._apply_icon_theme()
 
         
     def toggleCollapsedState(self):
         self._is_collapsed = not self._is_collapsed
         
         self.applyCollapsedState(self._is_collapsed)
-
+        
 
     def applyCollapsedState(self, collapsed):
         if collapsed:
             self.setFixedWidth(self.COLLAPSED_WIDTH)
-            # Just A test
-            self.sample.setCollapsed(True)
-            
+            for btn in self._buttons:
+                btn.setCollapsed(True)
+
         else:
             self.setFixedWidth(self.EXPANDED_WIDTH)
-            # Just A test
-            self.sample.setCollapsed(False)
+            for btn in self._buttons:
+                btn.setCollapsed(False)
             
     # --------------------------------------------------
     # Theme helpers
@@ -259,41 +305,28 @@ class NavigationRail(QWidget):
         v = theme_vars()
 
         # Define colors
-        color_inactive = v.qcolor("on_surface_variant")
-        color_active = v.qcolor("primary")
-        # Shutter icon sits on 'primary' background, so use 'on_primary' (usually white/black)
-        color_on_primary = v.qcolor("on_primary")
+        on_secondary_container = v.qcolor("on_secondary_container")
+        on_surface = v.qcolor("on_surface")
+        on_surface_variant = v.qcolor("on_surface_variant")
         
-        ico_menu = self._create_colored_icon("menu.svg", color_active)
-
         # WORKING METHOD SET A PIXMAP TO A QLabel AND SET SIZE 24 24
         # print(ico_menu)
         # self.menu_btn.setPixmap(ico_menu.pixmap(24, 24))
-
-        self.ico_menu_open_themed = self._create_colored_icon("menu_open.svg", color_active)
-
-        ico_dashboard_themed = self._create_colored_icon("dashboard.svg", color_active)
-        ico_dashboard_filled_themed = self._create_colored_icon("dashboard_filled.svg", color_active)
-
-        ico_calendar_themed = self._create_colored_icon("calendar.svg", color_active)
-        ico_calendar_filled_themed = self._create_colored_icon("calendar_filled.svg", color_active)
-
-        ico_settings_themed = self._create_colored_icon("settings.svg", color_active)
-        ico_settings_filled_themed = self._create_colored_icon("settings_filled.svg", color_active)
-
-        # EXAMPLES FROM SHUTTERBAR
-        # 1. Light Button Icon
-        # If checked, use active color; otherwise inactive color.
-        # flash_color = color_active if self.light_btn.isChecked() else color_inactive
-        # self.light_btn.setIcon(self._create_colored_icon("light.svg", flash_color))
-
-        # # 2. Timer Icon (used in paintEvent)
-        # # Always use the inactive color for the "off" state icon
-        # self._timer_icon = self._create_colored_icon("timer.svg", color_inactive)
+        self.ico_menu = self._create_colored_icon(ico_menu, on_surface_variant)
         
-        # # 3. Shutter Icon
-        # self.shutter_btn.setIcon(self._create_colored_icon("shutter.svg", color_on_primary))
+        self.ico_menu_open = self._create_colored_icon(ico_menu_open, on_secondary_container)
 
-        # Trigger a repaint to show changes
+        self.ico_dashboard = self._create_colored_icon(ico_dashboard, on_surface_variant)
+        
+        self.ico_dashboard_filled = self._create_colored_icon(ico_dashboard_filled, on_secondary_container)
+
+        self.ico_calendar = self._create_colored_icon(ico_calendar, on_surface_variant)
+
+        self.ico_calendar_filled = self._create_colored_icon(ico_calendar_filled, on_secondary_container)
+
+        self.ico_settings = self._create_colored_icon(ico_settings, on_surface_variant)
+        
+        self.ico_settings_filled = self._create_colored_icon(ico_settings_filled, on_secondary_container)
+
         self.update()
 
